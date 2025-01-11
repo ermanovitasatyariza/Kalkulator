0da864de68e9a68b571e4761ed089c0886ed628c
ermanovitasatyariza <57171728+ermanovitasatyariza@users.noreply.github.com>
2025-01-11 11:49:00 +0700

    percobaan pertama

---
+++ tugaskalkulator.js
@@ -0,0 +1,24 @@
+function Penjumlahan(){
+    a = prompt ("Masukkan data pertama");
+    b = prompt ("Masukkan data kedua");
+    alert(a + "+" +  b + " = " + ( Number(a) + Number(b) ) );
+ }
+
+ function Pengurangan(){
+    a = prompt ("Masukkan data pertama");
+    b = prompt ("Masukkan data kedua");
+    alert(a + "+" + b + " = " + ( Number(a) - Number(b) ) );
+ }
+
+function Pembagian(){
+    a = prompt ("Masukkan data pertama");
+    b = prompt ("Masukkan data kedua");
+    alert(a + "+" + b + " = " + ( Number(a) / Number(b) ) );
+ }
+
+ 
+function Perkalian(){
+    a = prompt ("Masukkan data pertama");
+    b = prompt ("Masukkan data kedua");
+    alert(a + "+" + b + " = " + ( Number(a) * Number(b) ) );
+ }
